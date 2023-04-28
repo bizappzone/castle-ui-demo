88e37ce9dfaf6c115e8f9210d5b2e3a0bfa0fa38
aj <art@ampsire.com>
2023-04-28 13:53:41 -0400

    add dependancies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NGXLogger } from 'ngx-logger';
 
 @Component({
   selector: 'castle-ui-demo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private logger: NGXLogger) {}
   title = 'castle-ui-demo';
+  ngOnInit(): void {
+    this.logger.trace('**** DS - button view');
+  }
+
+  onClick(value: any) {
+    this.logger.trace('$$$ BUTTON CLICKED $$$'), value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NgxCastleUiModule } from '@bizappzone/ngx-castle-ui';
+import { environment } from './environments/environment.prod';
+import { HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { LoggerModule } from 'ngx-logger';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -11,6 +21,20 @@ import { NgxCastleUiModule } from '@bizappzone/ngx-castle-ui';
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
     NgxCastleUiModule,
+    LoggerModule.forRoot({
+      serverLoggingUrl: `${environment.apiUrl}api/logs`,
+      level: environment.logLevel,
+      serverLogLevel: environment.serverLogLevel,
+      disableConsoleLogging: false,
+    }),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'en',
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
